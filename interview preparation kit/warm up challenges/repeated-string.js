7da9362ddb6b09d387c5dae3f73e42e3bed20d6c
Pandu Akas <panduakas@MacBook-Air-Pandu.local>
2020-01-07 14:23:16 +0700

    repeated string

--- interview preparation kit/warm up challenges/repeated-string.js
+++ interview preparation kit/warm up challenges/repeated-string.js
@@ -1,6 +1,23 @@
-const repeatedString = () => {
-
+const repeatedString = (s, n) => {
+  let count = 0;
+  s.split('').forEach(element => {
+    if (element === 'a') count++;
+  })
+  let result = Math.trunc(n / s.length) * count;
+  const sisa = n % s.length;
+  if (sisa !== 0) {
+    const stringSisa = s.slice(0, sisa);
+    stringSisa.split('').forEach(element => {
+      if (element === 'a') result++
+    })
+  }
+  return result;
 };
 
-const result = repeatedString();
+const string = 'aba';
+const string2 = 'abcac';
+const string3 = 'a';
+const n = 10;
+const n2 = 1000000000;
+const result = repeatedString(string, n);
 console.log(result);
